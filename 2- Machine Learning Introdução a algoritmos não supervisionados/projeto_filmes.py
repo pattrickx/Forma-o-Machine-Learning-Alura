43fafbd55e8ae23002469cb0f6cc930e2fdd2461
Patrick Martins <pattrickpx99@gmail.com>
2019-11-04 01:09:50 -0300

    2- Machine Learning Introdução a algoritmos não supervisionados aula 4
    Qual o problema de termos muitos ou poucos grupos
    Como é calculado o erro do modelo
    O que é o elbow method
    Como otimizar o número de clusters para o K-Means

--- 2- Machine Learning Introdução a algoritmos não supervisionados/projeto_filmes.py
+++ 2- Machine Learning Introdução a algoritmos não supervisionados/projeto_filmes.py
@@ -31,12 +31,13 @@ grupos = pd.DataFrame(modelo.cluster_centers_, columns=generos.columns)
 
 # print(grupos)
 # grupos.plot.bar()
-grupos.transpose().plot.bar(subplots=True,sharex=False) ## transpose() faz a matriz transposta 
+grupos.transpose().plot.bar(subplots=True,sharex=False, rot=0) ## transpose() faz a matriz transposta 
 plt.show()
 grupo = 0
 filtro=(modelo.labels_==grupo)
 #print(dados_filmes[filtro].sample(10)) #mostra parte dos dados de um dos grupos para verificação
-
+############### ajuste de dimençoes
+'''
 from sklearn.manifold import TSNE
 
 tsne=TSNE()
@@ -47,7 +48,41 @@ import seaborn as sns
 sns.set(rc={'figure.figsize':(13,13)})
 sns.scatterplot(x=visualizacao[:,0],y=visualizacao[:,1] , hue= modelo.labels_, palette=sns.color_palette('Set1',3))
 plt.show()
-
+'''
 # plt.scatter(visualizacao[:,0], visualizacao[:,1],c=modelo.labels_ ,s=1) #metodo alternativo
 # plt.show()
 
+############### aula 4
+######## testando modelo com 20 grupos
+modelo =KMeans(n_clusters=20) ### define numero de clusters
+modelo.fit(generos_escalados) ## treina com os dados e gera grupos
+
+grupos = pd.DataFrame(modelo.cluster_centers_, columns=generos.columns)
+
+grupos.transpose().plot.bar(subplots=True,sharex=False, rot=0) ## transpose() faz a matriz transposta 
+plt.show()
+grupo = 0
+filtro=(modelo.labels_==grupo)
+print(dados_filmes[filtro].sample(10)) #mostra parte dos dados de um dos grupos para verificação
+def kmeans(numeros_de_clusters,generos):
+    modelo=KMeans(n_clusters=numeros_de_clusters)
+    modelo.fit(generos)
+    return [numeros_de_clusters,modelo.inertia_]
+print(kmeans(20,generos_escalados))
+resultado = [kmeans(numero_grupos,generos_escalados) for numero_grupos in range(1,41)]
+# metodo do cutuvelo 
+########### procurar ponto de curva
+resultado= pd.DataFrame(resultado, columns=['grupos', 'inertia'])
+resultado.inertia.plot(xticks=resultado.grupos)
+plt.show()
+############################### teste com quantidade de grupos "ideais"
+modelo =KMeans(n_clusters=17) ### define numero de clusters
+modelo.fit(generos_escalados) ## treina com os dados e gera grupos
+
+grupos = pd.DataFrame(modelo.cluster_centers_, columns=generos.columns)
+
+grupos.transpose().plot.bar(subplots=True,sharex=False, rot=0) ## transpose() faz a matriz transposta 
+plt.show()
+grupo = 0
+filtro=(modelo.labels_==grupo)
+print(dados_filmes[filtro].sample(10)) #mostra parte dos dados de um dos grupos para verificação
