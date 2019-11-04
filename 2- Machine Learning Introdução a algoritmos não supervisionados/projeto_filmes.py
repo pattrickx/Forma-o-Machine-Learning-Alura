f5a1a3a3ea4d615d7d3542891e96516249fc4cd5
Patrick Martins <pattrickpx99@gmail.com>
2019-11-03 22:53:48 -0300

    2- Machine Learning Introdução a algoritmos não supervisionados aula 2
    A criar um modelo K-Means
    Como o K-Means funciona
    O que são os centroides
    a visualização dos centroides e a sua classificação

--- 2- Machine Learning Introdução a algoritmos não supervisionados/projeto_filmes.py
+++ 2- Machine Learning Introdução a algoritmos não supervisionados/projeto_filmes.py
@@ -1,19 +1,24 @@
 import pandas as pd
 uri="https://raw.githubusercontent.com/alura-cursos/machine-learning-algoritmos-nao-supervisionados/master/movies.csv"
 dados= pd.read_csv(uri)
-print(dados.head())
+#print(dados.head())
 mapa = {
     "movieId": "filme_id",
     "title" : "titulo",
     "genres" :"generos"
 }
 dados= dados.rename(columns=mapa)
-print(dados.head())
+#print(dados.head())
 generos = dados.generos.str.get_dummies() ## pegando todos os generos dos filmes e tornando em colunas
 dados_filmes = pd.concat([dados,generos], axis=1)
-print(dados_filmes.head())
+#print(dados_filmes.head())
 ####### escalando dados 
 from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 generos_escalados=scaler.fit_transform(generos) 
-print(generos_escalados)
+#print(generos_escalados)
+#########################################   AULA 2
+from sklearn.cluster import KMeans ### lib de clusterisação
+modelo =KMeans(n_clusters=3) ### define numero de clusters
+modelo.fit(generos_escalados) ## treina com os dados e gera grupos
+print('Grupos{}'.format(modelo.labels_)) ## mostra gupos
